d1e0f7d663fc5bbf5a7a662624ba9328cbd43b46
spestana <spestana@uw.edu>
2021-04-05 16:26:52 -0700

    Update plotting for summary stats

--- resampled_stats.py
+++ resampled_stats.py
@@ -11,6 +11,58 @@ import scipy.stats as stats
 import xarray as xr
 import matplotlib.pyplot as plt
 
+def clean_nans_from_dict(d):
+    '''For a dictionary where each key holds a list or np.array of values, remove all nan values.
+    Based on: https://stackoverflow.com/questions/24068306/is-there-a-way-to-remove-nan-from-a-dictionary-filled-with-data'''
+    
+    # create empty dict to put the cleaned-up dictionary key:value pairs
+    cleaned_d = {}
+    
+    # loop through each key in the original dict
+    for key_name in d:
+              
+        # for each item in the values for this key, if it is not nan then add to new list
+        items_cleaned = [item for item in d[key_name] if not np.isnan(item)]
+        
+        # add this list of cleaned items to the cleaned dictionary
+        cleaned_d[key_name] = items_cleaned
+
+    # return the cleaned dictionary
+    return cleaned_d
+
+
+def summary_stats(_a, _b):
+    '''Compute summary statistics for the difference between two sets.
+    Input two flattened (1-D) arrays with NaN values removed'''
+    
+    # remove nan values
+    a = _a[(np.isnan(_a)==False) & (np.isnan(_b)==False)]
+    b = _b[(np.isnan(_a)==False) & (np.isnan(_b)==False)]
+    
+    # for difference stats
+    diff = b - a
+    
+    # for linear regression stats
+    slope, intercept, r_value, p_value, std_err = stats.linregress(a, b)
+    
+    # populate dict with summary stats
+    summary_stats_dict = {
+        #'diff' : diff ,
+        'n' : len(diff) ,
+        'mean_diff' : np.nanmean( diff ),
+        'median_diff' : np.nanmedian( diff ),
+        'mean_squared_diff' : np.nanmean( diff**2 ),
+        'rms_diff' : np.sqrt( np.nanmean( diff**2 ) ),
+        'std_diff' : np.nanstd( diff ),
+        'slope' : slope,
+        'intercept' : intercept,
+        'r_value' : r_value,
+        'p_value' : p_value,
+        'std_err' : std_err
+        }
+    
+    return summary_stats_dict
+
 def compute_modes(resampled, n=0):
     
     '''Given a resampled xarray object (xarray.core.resample.DataArrayResample),
@@ -58,8 +110,11 @@ def resampled_stats(resampled, n=0, q=(.25,.75)):
          'stds': resampled.std().values,
          'qUpper': resampled.quantile(q[1]).values,
          'qLower': resampled.quantile(q[0]).values,}
-    
-    df = pd.DataFrame(data=d, index=resampled_modes.index)
+    # remove any nans from the dictionary
+    cleaned_d = clean_nans_from_dict(d)
+
+    # Create a dataframe from the cleaned-up dictionary
+    df = pd.DataFrame(data=cleaned_d, index=resampled_modes.index)
     
     return df
 
@@ -84,9 +139,9 @@ def resampled_plot(original_df, resampled_df, ymin=-20, ymax=20, xmin=0, xmax=40
     
     ### Resampled Dataframe, Timeseries "Boxplots" ###
     # mean marker
-    resampled_df.means.plot(linestyle='none',marker='o',color='k', label='Mean', ax=ax[1])
+    resampled_df.means.plot(linestyle='none',marker='_',color='k', label='Mean', ax=ax[1])
     # median marker
-    resampled_df.medians.plot(linestyle='none',marker='_',color='k', label='Median', ax=ax[1])
+    resampled_df.medians.plot(linestyle='none',marker='o',color='k', label='Median', ax=ax[1])
     # mode marker
     resampled_df.modes.plot(linestyle='none',marker='o',markerfacecolor='w',markeredgecolor='k', label='Mode', ax=ax[1])
     
@@ -96,13 +151,13 @@ def resampled_plot(original_df, resampled_df, ymin=-20, ymax=20, xmin=0, xmax=40
                 yerr=np.array([np.abs(resampled_df.qLower-resampled_df.means), 
                                np.abs(resampled_df.qUpper-resampled_df.means)]),
                 fmt='none',
-                linewidth=1,
+                linewidth=4,
                 color='k',
-                alpha=0.4,
+                alpha=0.3,
                 capsize=None,
                 label='IQR')
     
-    ax[1].axhline(0,linestyle='-',color='lightgrey',linewidth=1)
+    ax[1].axhline(0,linestyle='-',color='k',linewidth=1)
     plt.legend()
     
     # Format shared y-axis
